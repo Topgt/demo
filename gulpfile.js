e2ec77e78a104c669d6f70841ba2340c3b03695e
topgt1 <topgt1@163.com>
2016-10-18 01:21:55 +0800

    准备添加头部模块

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 
 var jshint = require('gulp-jshint'); //js语法规范检查
 var browserSync = require('browser-sync').create();
-var watch = require('gulp-watch');
 //var reqjsConfing = require('./reqjsConfing');  
 
 gulp.task('hellow', function() {
@@ -18,10 +17,9 @@ gulp.task('jshint', function() {
 
 gulp.task('browserSync', function(){
 	return browserSync.init({
-		port: '3004',
-		proxy: 'http://localhost:3000/src/views/login/login.html'
-	});
-	gulp.watch('.public/src/**').on('change', browserSync.reload);
+		port: '3000',
+		proxy: 'http://localhost:3001/src/views/login/login.html'
+	});	
 });
 
-
+gulp.watch('./public/src/**/*').on('change', browserSync.reload);
